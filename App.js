76f677417a32ad708e1d0ffe81742cece45f0760
ngocphuocha <phuoc04012000@gmail.com>
2022-09-27 06:46:18 +0700

    Passing params to a previous screen

--- App.js
+++ App.js
@@ -1,73 +1,70 @@
-import { Button, Text, View } from "react-native";
+import { Button, Text, View, TextInput } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useEffect, useState } from "react";
 
-function HomeScreen({ navigation }) {
+function HomeScreen({ route, navigation }) {
+  /** Getting the params */
+
+  useEffect(() => {
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+    }
+  }, [route.params?.post]);
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
       <Button
-        /* 1. Navigate to the Details route with params */
-        title="Go to Details"
-        onPress={() =>
-          navigation.navigate("Details", {
-            itemId: 86,
-            otherParam: "anything you want here",
-          })
-        }
+        title="Create Post"
+        onPress={() => navigation.navigate("CreatePost")}
       />
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
     </View>
   );
 }
 
-function DetailsScreen({ route, navigation: { setParams } }) {
-  /* 2. Get the param */
-  const { itemId, otherParam } = route.params;
+const CreatePostScreen = ({ navigation, route }) => {
+  const [postText, setPostText] = useState("");
 
-  const handleSetParamRoute = () => {
-    setParams({
-      itemId: 10,
-      otherParam: "Bo may test OK?",
+  const sendPropToHomeScreen = () => {
+    navigation.navigate({
+      name: "Home",
+      params: {
+        post: postText,
+      },
+      merge: false,
     });
   };
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-      <Text>itemId: {JSON.stringify(itemId)}</Text>
-      <Text>Other param: {JSON.stringify(otherParam)}</Text>
-      <Button
-        title="Go to Details... again ..."
-        onPress={() =>
-          navigation.push("Details", {
-            itemId: Math.floor(Math.random() * 100),
-            otherParam: `Other param: ${Math.floor(Math.random() * 100)}`,
-          })
-        }
+    <View style={{ padding: 30 }}>
+      <TextInput
+        style={{
+          height: 50,
+          padding: 10,
+          backgroundColor: "#fff",
+          borderRadius: 10,
+        }}
+        value={postText}
+        onChangeText={setPostText}
       />
-      <View style={{ marginTop: 20 }}></View>
-      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button title="Set param again" onPress={handleSetParamRoute} />
+      <Button title="Done" onPress={sendPropToHomeScreen} />
     </View>
   );
-}
+};
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator mode="modal">
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ title: "OverView" }}
         />
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-          initialParams={{ itemId: 1000, otherParam: "this is init param" }}
-        />
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
